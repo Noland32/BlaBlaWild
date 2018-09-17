48dae9f404a464db9d2079e355d5e929d77ad998
wilder <aymard.ph@gmail.com>
2018-09-17 15:27:54 +0200

    Modif avec le calendar

--- app/src/main/java/fr/wildcodeschool/blablawild/ItinerarySearchActivity.java
+++ app/src/main/java/fr/wildcodeschool/blablawild/ItinerarySearchActivity.java
@@ -1,13 +1,20 @@
 package fr.wildcodeschool.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 public class ItinerarySearchActivity extends AppCompatActivity {
 
@@ -43,8 +50,49 @@ public class ItinerarySearchActivity extends AppCompatActivity {
                     myIntent.putExtra(EXTRA_MESSAGE, message); //call l'intent et on put l'extra
                     startActivity(myIntent);
                 }
-
             }
         });
+
+
+            final Calendar myCalendar = Calendar.getInstance();
+
+            final EditText editDate = findViewById(R.id.Date);
+            final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+                private void updateLabel() {
+                    String myFormat = "MM/dd/yy"; //In which you need put here
+                    SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                    editDate.setText(sdf.format(myCalendar.getTime()));
+                }
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                      int dayOfMonth) {
+                    // TODO Auto-generated method stub
+                    myCalendar.set(Calendar.YEAR, year);
+                    myCalendar.set(Calendar.MONTH, monthOfYear);
+                    myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    updateLabel();
+                }
+            };
+
+            editDate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    new DatePickerDialog(ItinerarySearchActivity.this, date, myCalendar
+                            .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                            myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+
+                }
+            });
+
+
+
+
+
+
+
+
     }
 }
